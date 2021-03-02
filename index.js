22660a6b69898c59910a770c20f0f7098b2247cb
Alexander Babel <ab@marmalade.group>
2021-03-02 15:18:20 +0100

    refactor: update formatting

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const verifyConditions = require('./lib/verify')
-const publish = require('./lib/publish')
-const prepare = require('./lib/prepare')
+const verifyConditions = require("./lib/verify");
+const publish = require("./lib/publish");
+const prepare = require("./lib/prepare");
 
 module.exports = {
   verifyConditions,
   publish,
-  prepare
-}
+  prepare,
+};
